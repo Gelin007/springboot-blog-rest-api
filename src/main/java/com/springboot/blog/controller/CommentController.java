66b212c0b6944712cbae1681a269c99271b55163
Gelin Nguedong <gelinhope@hotmail.com>
2022-12-07 00:16:22 +0100

    - Bean Validation API dependency added - Validation Create Post and Comment REST API Request implemented - Validation Update Post and Comment REST API Request implemented

--- src/main/java/com/springboot/blog/controller/CommentController.java
+++ src/main/java/com/springboot/blog/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.springboot.blog.controller;
 
 import com.springboot.blog.dto.CommentDto;
 import com.springboot.blog.service.CommentService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +25,7 @@ public class CommentController {
 
     @PostMapping("/posts/{postId}/comments")
     public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId") long postId,
-                                                    @RequestBody CommentDto commentDto) {
+                                                    @Valid @RequestBody CommentDto commentDto) {
         return new ResponseEntity<>(commentService.createComment(postId, commentDto), HttpStatus.CREATED);
     }
 
@@ -43,7 +44,7 @@ public class CommentController {
     @PutMapping("/posts/{postId}/comments/{id}")
     public ResponseEntity<CommentDto> updateComment(@PathVariable(value = "postId") long postId,
                                                     @PathVariable(value = "id") long commentId,
-                                                    @RequestBody CommentDto commentDto) {
+                                                    @Valid @RequestBody CommentDto commentDto) {
 
         CommentDto updatedComment = commentService.updateComment(postId, commentId, commentDto);
         return ResponseEntity.ok(updatedComment);
--- src/main/java/com/springboot/blog/dto/CommentDto.java
+++ src/main/java/com/springboot/blog/dto/CommentDto.java
@@ -1,5 +1,8 @@
 package com.springboot.blog.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 /**
@@ -9,7 +12,15 @@ import lombok.Data;
 @Data
 public class CommentDto {
     private Long id;
+
+    @NotEmpty(message = "Comment name should not be null or empty")
     private String name;
+
+    @NotEmpty(message = "Comment email should not be null or empty")
+    @Email
     private String email;
+
+    @NotEmpty(message = "Comment body should not be null or empty")
+    @Size(min = 10, message = "Comment body should have at least 10 characters")
     private String body;
 }
