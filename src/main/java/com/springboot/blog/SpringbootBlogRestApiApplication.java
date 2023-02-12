3f746ba6adc196c79e5e4ed18f4ce550bd242733
Gelin Nguedong <gelinhope@hotmail.com>
2023-02-12 18:18:42 +0100

    - Write a code to insert Metadata in Tables - Add some profiles (dev, qa, prod)

--- src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -1,20 +1,38 @@
 package com.springboot.blog;
 
+import com.springboot.blog.entity.Role;
+import com.springboot.blog.repository.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringbootBlogRestApiApplication {
+public class SpringbootBlogRestApiApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
-	}
+    @Autowired
+    private RoleRepository roleRepository;
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
+    }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Override
+    public void run(String... args) {
+
+        Role adminRole = new Role();
+        adminRole.setName("ROLE_ADMIN");
+        roleRepository.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setName("ROLE_USER");
+        roleRepository.save(userRole);
+    }
 }
